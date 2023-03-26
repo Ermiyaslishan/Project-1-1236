aad6ca90583bd98be1d6c889929d1f1fc589a5a7
Ermiyaslishan <ermiyasyamlak3030@gmail.com>
2023-03-26 09:36:22 +0300

    Committed By Ermiyas Lishan

--- app/src/main/java/com/example/billcalculator/MainActivity.java
+++ app/src/main/java/com/example/billcalculator/MainActivity.java
@@ -1,20 +1,36 @@
 package com.example.billcalculator;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
+    private TextView TotalConsumptionDisplay;
+    private TextView TotalPayableDisplay;
+
+    private boolean calculationsPerformed = false;
+
+
+  @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+      this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
+        //we import all elemnt from XML file by using Id
 
 
     }
