0000a9ebc948e3f3cdb9956c667dd7317cfc66b2
Ermiyaslishan <ermiyasyamlak3030@gmail.com>
2023-03-26 10:36:33 +0300

    Committed By Ermiyas Lishan

--- app/src/main/java/com/example/billcalculator/MainActivity.java
+++ app/src/main/java/com/example/billcalculator/MainActivity.java
@@ -66,5 +66,164 @@ public class MainActivity extends AppCompatActivity {
       Button btnReset = findViewById(R.id.button3);
 
 
+    Checkbox1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if (Checkbox1.isChecked()){
+
+          editText1.setEnabled(true);
+          editText9.setEnabled(true);
+          editText5.setEnabled(true);
+        }else {
+          editText1.setEnabled(false);
+          editText1.getEditText().getText().clear();
+          editText9.setEnabled(false);
+          editText9.getEditText().getText().clear();
+          editText5.setEnabled(false);
+          editText5.getEditText().getText().clear();
+        }
+
+      }
+    });
+
+    Checkbox2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if (Checkbox2.isChecked()){
+          editText2.setEnabled(true);
+          editText10.setEnabled(true);
+          editText6.setEnabled(true);
+        }else {
+          editText2.setEnabled(false);
+          editText2.getEditText().getText().clear();
+          editText10.setEnabled(false);
+          editText10.getEditText().getText().clear();
+          editText6.setEnabled(false);
+          editText6.getEditText().getText().clear();
+        }
+
+      }
+    });
+
+    Checkbox3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if (Checkbox3.isChecked()){
+          editText3.setEnabled(true);
+          editText11.setEnabled(true);
+          editText7.setEnabled(true);
+        }else {
+          editText3.setEnabled(false);
+          editText3.getEditText().getText().clear();
+          editText11.setEnabled(false);
+          editText11.getEditText().getText().clear();
+          editText7.setEnabled(false);
+          editText7.getEditText().getText().clear();
+        }
+
+      }
+    });
+
+    Checkbox4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+      @Override
+      public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+        if (Checkbox4.isChecked()){
+          editText4.setEnabled(true);
+          editText12.setEnabled(true);
+          editText8.setEnabled(true);
+        }else {
+          editText4.setEnabled(false);
+          editText4.getEditText().getText().clear();
+          editText12.setEnabled(false);
+          editText12.getEditText().getText().clear();
+          editText8.setEnabled(false);
+          editText8.getEditText().getText().clear();
+        }
+
+      }
+    });
+
+
+    btnCalulate.setOnClickListener(new View.OnClickListener(){
+//          @SuppressLint("SetTextI18n")
+
+      @Override
+      public void onClick(View v) {
+        double lightBulbTotalConsumption = 0.0;
+        double ovenTotalConsumption = 0.0;
+        double boilerTotalConsumption = 0.0;
+        double mitadTotalConsumption = 0.0;
+
+        if (Checkbox1.isChecked()) {
+          double power = Double.parseDouble(editText1.getEditText().getText().toString());
+          double quantity = Double.parseDouble(editText9.getEditText().getText().toString());
+          double usage = Double.parseDouble(editText5.getEditText().getText().toString());
+
+          lightBulbTotalConsumption = power * quantity * usage * 30;
+        }
+        if (Checkbox2.isChecked()) {
+          double power = Double.parseDouble(editText2.getEditText().getText().toString());
+          double quantity = Double.parseDouble(editText10.getEditText().getText().toString());
+          double usage = Double.parseDouble(editText6.getEditText().getText().toString());
+
+          ovenTotalConsumption = power * quantity * usage * 30;
+        }
+        if (Checkbox3.isChecked()) {
+          double power = Double.parseDouble(editText3.getEditText().getText().toString());
+          double quantity = Double.parseDouble(editText11.getEditText().getText().toString());
+          double usage = Double.parseDouble(editText7.getEditText().getText().toString());
+
+          boilerTotalConsumption = power * quantity * usage * 30;
+        }
+        if (Checkbox4.isChecked()) {
+          double power = Double.parseDouble(editText4.getEditText().getText().toString());
+          double quantity = Double.parseDouble(editText12.getEditText().getText().toString());
+          double usage = Double.parseDouble(editText8.getEditText().getText().toString());
+
+          mitadTotalConsumption = power * quantity * usage;
+        }
+        double allConsumption = (mitadTotalConsumption + boilerTotalConsumption
+                + lightBulbTotalConsumption + ovenTotalConsumption) / 1000;
+//              double allConsumption = Math.round(allConsumption1);
+        if (allConsumption == 0) {
+          TotalPayableDisplay.setText("0.0");
+        }else {
+          TotalConsumptionDisplay.setText(String.format("%sKwh", Double.toString(allConsumption)));
+        }
+        if (allConsumption == 0) {
+          TotalPayableDisplay.setText("0.0");
+        } else {
+          double payable = 0.0;
+          if (allConsumption <= 50) {
+            payable = Math.round((allConsumption * 0.2730)) + 10.00;
+
+          } else if (allConsumption > 50 && allConsumption <= 100) {
+            payable = Math.round((allConsumption * 0.7670)) + 42.00;
+          } else if (allConsumption > 100 && allConsumption <= 200) {
+            payable = Math.round((allConsumption * 1.6250)) + 42.00;
+          } else if (allConsumption > 200 && allConsumption <= 300) {
+            payable = Math.round((allConsumption * 2.0000)) + 42.00;
+          } else if (allConsumption > 300 && allConsumption <= 400) {
+            payable = Math.round((allConsumption * 2.2000)) + 42.00;
+          } else if (allConsumption > 400 && allConsumption <= 500) {
+            payable = Math.round((allConsumption * 2.4050)) + 42.00;
+          } else if (allConsumption > 500) {
+            payable = Math.round((allConsumption * 2.4810)) + 42.00;
+          }
+
+
+          // Display the result
+          TotalPayableDisplay.setText(Double.toString(payable) + " Birr");
+//              TotalPayableDisplay.setText(Double.toString(payable)+ "Birr");
+
+        }
+      }
+
+    });
+
     }
 }
